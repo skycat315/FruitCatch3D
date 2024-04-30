20e08859bdbca54aa1d83ada2d13b149f618c5c8
skycat315 <ykk4315@yahoo.co.jp>
2024-04-29 22:34:12 -0400

    First Commit

---
+++ Assets/GameManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // リトライ処理
+    public void Retry() {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
